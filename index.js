6cd15ad9458c7729b27699b78a9ca57d43531517
Sachin Gupta <64950764+sachin-prog@users.noreply.github.com>
2024-10-17 17:06:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.get("/", (req, res) => {
     res.status(200)
-    res.end("<div><p>Hello Jenkins_CI-CD !<p></div>");
+    res.end("<div><p>Hello Sachin Welcome to Devops !<p></div>");
 })
 
 //Starting server at 4000
